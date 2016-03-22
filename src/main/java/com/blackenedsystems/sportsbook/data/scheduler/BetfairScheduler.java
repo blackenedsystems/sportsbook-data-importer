bab1fe27c7fd5680435a2dbba1b19fd9811a2293
blackenedsystems <alan@blackenedsystems.com>
2016-03-22 17:15:38 +0100

    Remove Akka classes. Synchronous load of data.

--- src/main/java/com/blackenedsystems/sportsbook/data/scheduler/BetfairScheduler.java
+++ src/main/java/com/blackenedsystems/sportsbook/data/scheduler/BetfairScheduler.java
@@ -1,17 +1,30 @@
 package com.blackenedsystems.sportsbook.data.scheduler;
 
-import akka.actor.ActorRef;
-import com.blackenedsystems.sportsbook.data.akka.ActorService;
+import com.blackenedsystems.sportsbook.data.betfair.BetfairClient;
 import com.blackenedsystems.sportsbook.data.betfair.BetfairConfiguration;
-import com.blackenedsystems.sportsbook.data.betfair.akka.BetfairDataImportActor;
-import com.blackenedsystems.sportsbook.data.betfair.akka.ProcessType;
+import com.blackenedsystems.sportsbook.data.betfair.BetfairConnector;
+import com.blackenedsystems.sportsbook.data.betfair.BetfairDataMappingService;
+import com.blackenedsystems.sportsbook.data.betfair.model.Competition;
+import com.blackenedsystems.sportsbook.data.betfair.model.Event;
+import com.blackenedsystems.sportsbook.data.betfair.model.EventType;
+import com.blackenedsystems.sportsbook.data.betfair.model.MarketType;
+import com.blackenedsystems.sportsbook.data.mapping.DataMappingService;
+import com.blackenedsystems.sportsbook.data.mapping.model.DataMapping;
+import com.blackenedsystems.sportsbook.data.mapping.model.ExternalDataSource;
+import com.blackenedsystems.sportsbook.data.mapping.model.MappingType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author Alan Tibbetts
@@ -23,42 +36,94 @@ public class BetfairScheduler {
     private static final Logger LOGGER = LoggerFactory.getLogger(BetfairScheduler.class);
 
     @Autowired
-    private ActorService actorService;
+    private BetfairConnector betfairConnector;
+
+    @Autowired
+    private BetfairClient betfairClient;
+
+    @Autowired
+    private BetfairDataMappingService betfairDataMappingService;
 
     @Autowired
     private BetfairConfiguration betfairConfiguration;
 
-    private ActorRef dataImportActor;
-    private int iteration = 0;
+    @Autowired
+    private DataMappingService dataMappingService;
 
-    @PostConstruct
-    public void createActors() {
-        dataImportActor = actorService.actorFromContext("BetfairDataImportActor", "betfairDataImportActor");
-    }
+    ExecutorService executorService = Executors.newFixedThreadPool(30);
+    private int iteration = 0;
 
     @Scheduled(cron = "${schedule.betfair.odds}")
     public void loadData() {
+        LocalDateTime start = LocalDateTime.now();
         try {
-            BetfairDataImportActor.Start msg = constructStartMessage();
-            dataImportActor.tell(msg, dataImportActor);
+            betfairConnector.logon();
+            loadBaseData();
+            loadEventData();
 
             iteration++;
         } catch (Exception e) {
             LOGGER.error("Error", e);
+        } finally {
+            LOGGER.info("Betfair data load completed in {} seconds.", start.until(LocalDateTime.now(), ChronoUnit.SECONDS));
+
+            if (betfairConnector.isConnected()) {
+                try {
+                    betfairConnector.logout();
+                } catch (Exception e) {
+                    LOGGER.error("Failed to logout from Betfair", e);
+                }
+            }
         }
     }
 
-    private BetfairDataImportActor.Start constructStartMessage() {
-        BetfairDataImportActor.Start msg = new BetfairDataImportActor.Start();
-        if (betfairConfiguration.loadMarketsAndOdds) {
-            msg.process(ProcessType.ODDS);
-        }
+    private void loadEventData() throws IOException {
         if (iteration % betfairConfiguration.eventDataInterval == 0) {
-            msg.process(ProcessType.EVENTS);
+            loadCompetitions();
+            loadEvents();
         }
-        if (iteration % betfairConfiguration.baseDataInterval == 0) {
-            msg.process(ProcessType.BASE);
-        }
-        return msg;
     }
+
+    private void loadCompetitions() throws IOException {
+        List<DataMapping> categoryMappings = dataMappingService.loadDataMappingsMarkedForProcessing(ExternalDataSource.BETFAIR, MappingType.CATEGORY);
+
+        for (DataMapping categoryMapping : categoryMappings) {
+            List<Competition> competitions = betfairClient.loadCompetitions(categoryMapping.getExternalId());
+            if (competitions.size() > 0) {
+                betfairDataMappingService.processCompetitionList(categoryMapping.getExternalDescription(), competitions);
+            }
+        }
+
+    }
+
+    /**
+     * For each competition mapping marked as active, load associated events.  It is necessary to load events one
+     * competition at a time because Betfair do not return any indication of the event's parent, and if we want to
+     * automatically generate events in the internal model, we need to now the competition to which it belongs.
+     *
+     * This has implications as to how many competitions should be active with Betfair.  Though in any case, Befair should
+     * probably not be the primary data source!
+     */
+    private void loadEvents() throws IOException {
+        List<DataMapping> competitionMappings = dataMappingService.loadDataMappingsMarkedForProcessing(ExternalDataSource.BETFAIR, MappingType.COMPETITION);
+
+        for (DataMapping competitionMapping : competitionMappings) {
+            HashSet<String> idSet = new HashSet<>();
+            idSet.add(competitionMapping.getExternalId());
+
+            List<Event> eventList = betfairClient.loadEvents(idSet);
+            betfairDataMappingService.processEventList(competitionMapping.getExternalDescription(), eventList);
+        }
+    }
+
+    private void loadBaseData() throws IOException {
+        if (iteration % betfairConfiguration.baseDataInterval == 0) {
+            List<EventType> eventTypes = betfairClient.loadEventTypes();
+            betfairDataMappingService.processEventTypeList(eventTypes);
+
+            List<MarketType> marketTypes = betfairClient.loadMarketTypes();
+            betfairDataMappingService.processMarketTypeList(marketTypes);
+        }
+    }
+
 }
